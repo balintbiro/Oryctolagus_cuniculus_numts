0f2d3b22f31b9946a8c8137e6e2eb4d7837649d2
balintbiro <biro.balint.szie@gmail.com>
2021-12-07 10:27:48 +0100

    Tsuji et al. (2012) threshold
    Using the e value threshold proposed by Tsuji et al., (2012) instead of our reversed mt method.

--- codes/5_significant_alignments.py
+++ codes/5_significant_alignments.py
@@ -1,15 +1,8 @@
 #import the required modules
 import pandas as pd
-
-#get the e values from the reversed mt alignment
-e_values=[]
-lines_not_in_use=[]
-with open('../data/reversed_mt_alignment.fa')as infile:
-    content=pd.Series(infile.readlines())
-    content.apply(lambda line: e_values.append(float(line.rsplit()[3].split('=')[1])) if 'EG2' in line else lines_not_in_use.append(line))
-    
+ 
 #e value threshold is going to be the lowest value of the r mt alignment
-e_threshold=min(e_values)
+e_threshold=10**-3
 
 #process the duplicated alignment and filter the signifcant alignments, then write them into a separated file
 with open('../data/duplicated_mt_alignment.fa')as infile, open('../results/significant_alignments.fa','w')as outfile:
