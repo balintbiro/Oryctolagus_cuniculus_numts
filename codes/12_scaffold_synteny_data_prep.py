a34ca49eae8cf3e08ee0f48eb39d159c9501aac2
balintbiro <biro.balint.szie@gmail.com>
2021-12-07 11:45:58 +0100

    delete old color codes

--- codes/12_scaffold_synteny_data_prep.py
+++ codes/12_scaffold_synteny_data_prep.py
@@ -58,63 +58,7 @@ mt_genes={}
 mt_annotation.apply(prepare_mt_genes, axis=1)
 mt_genes=pd.Series(mt_genes)
 
-#colorcodes for synteny
-#rapidtables.com/web/color/RGB_Color.html
-#https://r-charts.com/colors/
-colors=['FF0000',#red
-       'FFA500',#orange
-       'DAA520',#golden rod
-       'EEE8AA',#pale golden rod
-       'BDB76B',#dark khaki
-       '6B8E23',#olive drab
-       '228B22',#lime
-       '8FBC8F',#dark sea green
-       '00FFFF',#aqua
-       'AFEEEE',#pale turquoise
-       '7FFFD4',#aqua marine
-       'B0E0E6',#powder blue
-       '5F9EA0',#cadet blue
-       '6495ED',#corn flower blue
-       '00BFFF',#deep sky blue
-       '0000FF',#blue
-       '8A2BE2',#blue violet
-       'BA55D3',#medium orchid
-       '800080',#purple
-       'D8BFD8',#thistle
-       'DDA0DD',#plum
-       'FF00FF',#magenta
-       'FFC0CB',#pink
-       'F5F5DC',#beige
-       'F5DEB3',#wheat
-       'D2691E',#chocolate
-       'F4A460',#sandy brown
-       'BC8F8F',#rosy brown
-       'B0C4DE',#light steel blue
-       '000000',#black
-       'A9A9A9',#dark grey
-       'D3D3D3',#light grey
-       'FFDEAD',#navajo white
-       'FFE4E1',#misty rose
-       'FF69B4',#hot pink
-       'E0FFFF',#light cyan
-       'FFFF00',#yellow
-       'FFFACD',#lemon chiffon
-       'CD853F',#peru
-       'E6E6FA',#lavender
-       'C0C0C0',#silver
-       '1E90FF',#dodger blue
-       '98FB98',#pale green
-       '808000',#olive
-       'CD5C5C',#indian red
-       'BDB76B',#dark khaki
-       '2E8B57',#sea green
-       '20B2AA',#light sea green
-       'FF6347',#tomato
-       '191970',#midnight blue
-       '4169E1'#royal blue
-       ]
-
-#define colors in a randomized way
+#define colors in a randomized way for synteny analyses
 color_part=lambda: random.randint(0,255)
 color_ids=pd.Series(np.arange(1,100,1))
 colors=color_ids.apply(lambda number: '%02X%02X%02X' % (color_part(),color_part(),color_part())).tolist()
