818fd4d2f026c9f6c43c745a4484c845d3047a3d
balintbiro <biro.balint.szie@gmail.com>
2021-12-08 21:19:19 +0100

    original e value th

--- codes/5_significant_alignments.py
+++ codes/5_significant_alignments.py
@@ -2,7 +2,13 @@
 import pandas as pd
  
 #e value threshold is going to be the lowest value of the r mt alignment
-e_threshold=10**-3
+e_values=[]
+with open(f'../data/{organism_name}/r_mt_alignment.fa')as infile:
+    content=pd.Series(infile.readlines())
+    mask=content.apply(lambda line: 'EG2' in line)
+    content[mask].apply(lambda line: e_values.append(float(line.rsplit()[3].split('=')[1])))
+e_values.sort()
+e_threshold=e_values[0]
 
 #process the duplicated alignment and filter the signifcant alignments, then write them into a separated file
 with open('../data/duplicated_mt_alignment.fa')as infile, open('../results/significant_alignments.fa','w')as outfile:
