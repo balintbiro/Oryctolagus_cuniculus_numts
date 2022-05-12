7f8aa1b41f69dff59be4b40ea52b2d6271a8e215
Balint Biro <biro.balint.szie@gmail.com>
2022-05-12 12:03:33 +0200

    RMTransformation
    Pandas is not able to read RM output file so it is done in R.

--- codes/revision/repeatmasker.py
+++ codes/revision/repeatmasker.py
@@ -8,26 +8,36 @@ from subprocess import run,call
 alignments=pd.read_csv('../data/revision/filtered_alignments.csv',index_col=0)
 
 #function for aligning the sequences
-def pairwise_alignments(row,outfile):
-	try:
-		chr_id=row['chr_id']
-		chr_start=row['chr_start']
-		chr_end=row['chr_start']+row['chr_no_letters']
-		chr_part_size=row['chr_no_letters']
+def seq_preparation(row,outfile):
+	chr_id=row['chr_id']
+	chr_start=row['chr_start']
+	chr_end=row['chr_start']+row['chr_no_letters']
+	chr_part_size=row['chr_no_letters']
 
-		scf_id=row['scf_id']
-		scf_start=row['scf_start']
-		scf_end=row['scf_start']+row['scf_no_letters']
-		scf_part_size=row['scf_no_letters']
+	scf_id=row['scf_id']
+	scf_start=row['scf_start']
+	scf_end=row['scf_start']+row['scf_no_letters']
+	scf_part_size=row['scf_no_letters']
 
-		numtChr_w_flankings=''.join(str(run(f'samtools faidx ../data/Oryctolagus_cuniculus.OryCun2.0.dna.toplevel.fa {str(int(chr_id))}:{str(int(chr_start-chr_part_size))}-{str(int(chr_end+chr_part_size))}',shell=True,capture_output=True).stdout).split('\\n')[1:-1])
-		numtScf_w_flankings=''.join(str(run(f'samtools faidx ../data/Oryctolagus_cuniculus.OryCun2.0.dna.toplevel.fa {scf_id}:{str(int(scf_start-scf_part_size))}-{str(int(scf_end+scf_part_size))}',shell=True,capture_output=True).stdout).split('\\n')[1:-1])
-		outfile.write('>'+f'{str(int(chr_id))}:{str(int(chr_start-chr_part_size))}-{str(int(chr_end+chr_part_size))}'+'\n'+numtChr_w_flankings+'\n')
-		outfile.write('>'+f'{scf_id}:{str(int(scf_start-scf_part_size))}-{str(int(scf_end+scf_part_size))}'+'\n'+numtScf_w_flankings+'\n')
-	except:
-		print(row)
+	numtChr=''.join(str(run(f'samtools faidx ../data/Oryctolagus_cuniculus.OryCun2.0.dna.toplevel.fa {str(int(chr_id))}:{str(int(chr_start))}-{str(int(chr_end))}',shell=True,capture_output=True).stdout).split('\\n')[1:-1])
+	numtScf=''.join(str(run(f'samtools faidx ../data/Oryctolagus_cuniculus.OryCun2.0.dna.toplevel.fa {scf_id}:{str(int(scf_start))}-{str(int(scf_end))}',shell=True,capture_output=True).stdout).split('\\n')[1:-1])
+
+	upstreamChr=''.join(str(run(f'samtools faidx ../data/Oryctolagus_cuniculus.OryCun2.0.dna.toplevel.fa {str(int(chr_id))}:{str(int(chr_start-chr_part_size))}-{str(int(chr_start))}',shell=True,capture_output=True).stdout).split('\\n')[1:-1])
+	upstreamScf=''.join(str(run(f'samtools faidx ../data/Oryctolagus_cuniculus.OryCun2.0.dna.toplevel.fa {scf_id}:{str(int(scf_start-scf_part_size))}-{str(int(scf_start))}',shell=True,capture_output=True).stdout).split('\\n')[1:-1])
+
+	downstreamChr=''.join(str(run(f'samtools faidx ../data/Oryctolagus_cuniculus.OryCun2.0.dna.toplevel.fa {str(int(chr_id))}:{str(int(chr_end))}-{str(int(chr_end+chr_part_size))}',shell=True,capture_output=True).stdout).split('\\n')[1:-1])
+	downstreamScf=''.join(str(run(f'samtools faidx ../data/Oryctolagus_cuniculus.OryCun2.0.dna.toplevel.fa {scf_id}:{str(int(scf_end))}-{str(int(scf_end+scf_part_size))}',shell=True,capture_output=True).stdout).split('\\n')[1:-1])
+
+	outfile.write('>'+f'{str(int(chr_id))}:{str(int(chr_start))}-{str(int(chr_end))}_ChrNumt'+'\n'+numtChr+'\n')
+	outfile.write('>'+f'{scf_id}:{str(int(scf_start))}-{str(int(scf_end))}_ScfNumt'+'\n'+numtScf+'\n')
+
+	outfile.write('>'+f'{str(int(chr_id))}:{str(int(chr_start-chr_part_size))}-{str(int(chr_start))}_ChrUp'+'\n'+upstreamChr+'\n')
+	outfile.write('>'f'{scf_id}:{str(int(scf_start-scf_part_size))}-{str(int(scf_start))}_ScfUp'+'\n'+upstreamScf+'\n')
+
+	outfile.write('>'+f'{str(int(chr_id))}:{str(int(chr_end))}-{str(int(chr_end+chr_part_size))}_ChrDown'+'\n'+downstreamChr+'\n')
+	outfile.write('>'+f'{scf_id}:{str(int(scf_end))}-{str(int(scf_end+scf_part_size))}_ScfDown'+'\n'+downstreamScf+'\n')
 
 with open('../data/revision/rm_input.fa','w')as outfile:
-	alignments.apply(pairwise_alignments,args=(outfile,),axis=1)
+	alignments.apply(seq_preparation,args=(outfile,),axis=1)
 
 call('RepeatMasker -species "oryctolagus cuniculus" ../data/revision/rm_input.fa',shell=True)
